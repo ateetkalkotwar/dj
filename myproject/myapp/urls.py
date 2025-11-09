1eed8979fe0ace2efb7b8bed5d5055c5ab78dbec
ateetkalkotwar <158085385+ateetkalkotwar@users.noreply.github.com>
2025-11-10 02:17:38 +0530

    Delete myproject directory

--- myproject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views   # ✅ This imports your views.py
-
-urlpatterns = [
-    path('', views.student_form, name='student_form'),
-    path('success/', views.success, name='success'),
-]
